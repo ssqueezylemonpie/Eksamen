d35fd0b45f855b6e3064c2d8b177c2b5c28f7dc8
ssqueezylemonpie <stiansjerve@gmail.com>
2026-06-03 11:26:57 +0200

    ee

--- db.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def get_db_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="your_username",
-        password="your_password",
-        database="your_database"
-    )
